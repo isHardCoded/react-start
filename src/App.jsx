7b1bde262cf5254bf8169d880229ed561114068c
isHardCoded <ishardcoded@gmail.com>
2025-04-30 19:43:42 +0300

    Добавили пример использования хука useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
 import ProductList from './components/product-list'
 
+import React from 'react'
+
 function App() {
+	const [count, setCount] = React.useState(0)
+	const [open, setOpen] = React.useState(false)
+
+	console.log(open)
+
 	return (
-		<div className='container'>
-			<ProductList />
-		</div>
+		<>
+			<div className='container'>
+				<ProductList />
+			</div>
+
+			<p>Счетчик: {count}</p>
+			<button onClick={() => setCount(count + 1)}>Click</button>
+
+			{open && (
+				<div>
+					<p>Блок</p>
+				</div>
+			)}
+
+			<button onClick={() => setOpen(open => !open)}>
+				{open ? 'Закрыть блок' : 'Открыть блок'}
+			</button>
+		</>
 	)
 }
 
