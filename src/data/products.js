714269a5670f8f9f0de97ee309ffc0b588fd7806
isHardCoded <ishardcoded@gmail.com>
2025-04-30 18:12:49 +0300

    Вынесли массив с продуктами в отдельный файл

---
+++ src/data/products.js
@@ -0,0 +1,43 @@
+export const products = [
+	{
+		id: 1,
+		title: 'Phone',
+		description: 'About phone',
+		price: 449.99,
+	},
+
+	{
+		id: 2,
+		title: 'TV',
+		description: 'About TV',
+		price: 235.5,
+	},
+
+	{
+		id: 3,
+		title: 'Fridge',
+		description: 'About Fridge',
+		price: 99.99,
+	},
+
+	{
+		id: 4,
+		title: 'Playstation',
+		description: 'About Playstation',
+		price: 500.99,
+	},
+
+	{
+		id: 5,
+		title: 'Laptop',
+		description: 'About Laptop',
+		price: 725.5,
+	},
+
+	{
+		id: 6,
+		title: 'Mixer',
+		description: 'About Mixer',
+		price: 38.99,
+	},
+]
