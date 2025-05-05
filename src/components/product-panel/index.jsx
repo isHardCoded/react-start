5b9d13687cd81c305da6646d7ba0ae74dd2220ba
isHardCoded <ishardcoded@gmail.com>
2025-05-05 20:57:36 +0500

    Сделали разметку для меню в панели

--- src/components/product-panel/index.jsx
+++ src/components/product-panel/index.jsx
@@ -5,9 +5,16 @@ function ProductPanel() {
 	return (
 		<div className={styles.panel}>
 			<h2>Products</h2>
-			<div>
-				<span>Price: Low to High</span>
-				<img src={DownArrowIcon} alt='' />
+			<div className={styles.panelMenu}>
+				<div>
+					<span>Price: Low to High</span>
+					<img src={DownArrowIcon} alt='' />
+				</div>
+				<ul>
+					<li>Price: Low to High</li>
+					<li>Price: High to Low</li>
+					<li>Rating: High to Low</li>
+				</ul>
 			</div>
 		</div>
 	)
