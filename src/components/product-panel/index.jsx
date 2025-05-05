ae26c3215b0b3140a867b3a5a61db5df30460fe3
isHardCoded <ishardcoded@gmail.com>
2025-05-05 21:52:17 +0500

    Реализовали список фильтрации

--- src/components/product-panel/index.jsx
+++ src/components/product-panel/index.jsx
@@ -1,20 +1,42 @@
 import styles from './index.module.scss'
 import DownArrowIcon from '../../assets/icons/down-arrow-icon.svg'
+import React from 'react'
+
+const menuItems = [
+	'Price: Low to High',
+	'Price: High to Low',
+	'Rating: High to Low',
+]
 
 function ProductPanel() {
+	const [open, setOpen] = React.useState(false)
+	const [active, setActive] = React.useState(0)
+
+	function handleActiveItem(index) {
+		setActive(index)
+		setOpen(false)
+	}
+
 	return (
 		<div className={styles.panel}>
 			<h2>Products</h2>
 			<div className={styles.panelMenu}>
-				<div>
-					<span>Price: Low to High</span>
+				<div onClick={() => setOpen(open => !open)}>
+					<span>{active != null ? menuItems[active] : menuItems[0]}</span>
 					<img src={DownArrowIcon} alt='' />
 				</div>
-				<ul>
-					<li>Price: Low to High</li>
-					<li>Price: High to Low</li>
-					<li>Rating: High to Low</li>
-				</ul>
+				{open && (
+					<ul>
+						{menuItems.map((item, index) => (
+							<li
+								onClick={() => handleActiveItem(index)}
+								className={active == index ? styles.active : ''}
+							>
+								{item}
+							</li>
+						))}
+					</ul>
+				)}
 			</div>
 		</div>
 	)
