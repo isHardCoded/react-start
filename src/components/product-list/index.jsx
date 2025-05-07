51adcace58626b9b66ec0dc45a2bf032c6a3d39b
isHardCoded <ishardcoded@gmail.com>
2025-05-07 18:19:34 +0300

    Добавили API FakeStore, создали метод getData для получения товаров с сервера

--- src/components/product-list/index.jsx
+++ src/components/product-list/index.jsx
@@ -1,15 +1,29 @@
 import styles from './index.module.css'
-import { products } from '../../data/products'
 import Card from '../card'
+import React from 'react'
+
+const API_BASE = 'https://fakestoreapi.com/products'
 
 function ProductList() {
+	const [products, setProducts] = React.useState([])
+
+	const getData = async () => {
+		const response = await fetch(API_BASE)
+		const data = await response.json()
+		setProducts(data)
+	}
+
+	React.useEffect(() => {
+		getData()
+	}, [])
+
+	console.log(products)
+
 	return (
 		<>
 			<ul className={styles.productList}>
 				{products.map(product => (
-					<li key={product.id}>
-						<Card {...product} />
-					</li>
+					<li></li>
 				))}
 			</ul>
 		</>
